2bb530a03f5d672dd15d18213ef8626150dd4b9a
LGD-P <nplgdjp@gmx.com>
2023-05-31 17:46:01 +0200

    admin pretty display

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 from .models import Ticket
 
 
-admin.site.register(Ticket)
+# admin.site.register(Ticket)
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title', 'time_created']
