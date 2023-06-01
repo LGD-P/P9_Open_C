11c97b9363b0c57e168724c890e8fecb24ea7595
LGD-P <nplgdjp@gmx.com>
2023-06-01 16:57:49 +0200

    change signup form

--- litreview/authenticate/forms.py
+++ litreview/authenticate/forms.py
@@ -1,8 +1,26 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import get_user_model
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UsernameField
+from django.contrib.auth import get_user_model, password_validation
 
 
 class SignupForm(UserCreationForm):
+    username = UsernameField(label=False, widget=forms.TextInput(
+        attrs={"autofocus": True, "placeholder": "Nom d'utilisateur"}))
+    password1 = forms.CharField(label=False,
+                                strip=False,
+                                widget=forms.PasswordInput(
+                                    attrs={"autocomplete": "new-password", "placeholder": "Mot de passe"}),
+                                help_text=password_validation.password_validators_help_text_html(),
+                                )
+    password2 = forms.CharField(label=False,
+                                widget=forms.PasswordInput(
+                                    attrs={"autocomplete": "new-password",
+                                           "placeholder": "Confirmation du mot de passe"}),
+                                strip=False,
+                                help_text=(
+                                    "Enter the same password as before, for verification."),
+                                )
+
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = ["username", 'email']
+        fields = ["username"]
