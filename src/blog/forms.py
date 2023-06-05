244955f10cb54dd0e6e9d85b49179fc59bc49f89
LGD-P <nplgdjp@gmx.com>
2023-06-05 02:41:39 +0200

    rm -r src/

--- src/blog/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from . import models
-
-
-class TicketForms(forms.ModelForm):
-    class Meta:
-        model = models.Ticket
-        fields = ["title", "description", "image"]
